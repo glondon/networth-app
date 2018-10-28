4b26faf2749220b5f2f519d096038edb0dc04508
glondon <gregdlondon@gmail.com>
2018-10-28 15:13:42 -0500

    #6 began updating src directory for customizations

---
+++ src/Components/Assets.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Assets extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Assets">
+        Assets
+      </div>
+    );
+  }
+}
+
+export default Assets;
---
+++ src/Components/Options.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Options extends Component {
+  render() {
+    //console.log(this.props);
+    return (
+      <li className="Option">
+      
+      </li>
+    );
+  }
+}
+
+export default Options;
